56073a1f92ba5d9d956a15fc665256707d7dd1ed
Ojeranti08 <kemiola190@gmail.com>
2024-01-16 23:18:38 -0500

    fix files

--- c3-ec2instance.tf
+++ c3-ec2instance.tf
@@ -1,9 +1,9 @@
 # EC2 Instance Resources
-resource "aws_instance" "devops" {
+resource "aws_instance" "ecomm" {
   ami           = "ami-0005e0cfe09cc9050"
   instance_type = "t2.micro"
   key_name = "Oje"
   tags = {
-    Name = "Devops"
+    Name = "Ecomm"
 }
 }
