3a4e933a980b1e4202b7fcddbcd63bb350c45b0b
Ojeranti08 <kemiola190@gmail.com>
2024-01-17 01:07:33 -0500

    modified

--- c5-ansible.tf
+++ c5-ansible.tf
@@ -1,11 +1,11 @@
 # Creating Ansible Master EC2 Resources
 resource "aws_instance" "ansible_master" {
   ami                    = "ami-0005e0cfe09cc9050"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.ecomm-public-subnet.id
   key_name               = "Oje"
   vpc_security_group_ids = [aws_security_group.ecomm-sg.id]
-  user_data              = file["install-ansible.sh"]
+  user_data              = file("install-ansible.sh")
   private_ip             = "10.0.1.10"
 
   tags = {
@@ -20,9 +20,8 @@ resource "aws_instance" "ansible_node1" {
   subnet_id              = aws_subnet.ecomm-public-subnet.id
   key_name               = "Oje"
   vpc_security_group_ids = [aws_security_group.ecomm-sg.id]
-  user_data              = file["install-ansible.sh"]
-  private_ip             = "10.0.1.12"
-
+  user_data              = file("install-ansible.sh")
+  private_ip = "10.0.1.12"
   tags = {
     Name = "Ansible Node1"
   }
